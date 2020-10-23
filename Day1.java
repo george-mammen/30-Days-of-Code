13e4bed8ac28bfea7a6f58bf1d7b9587b1499c03
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2020-10-23 10:04:30 +0530

    Create Day1.java

---
+++ Day1.java
@@ -0,0 +1,17 @@
+public class Day1 {
+	public static void main(String[] args) {
+        
+		Scanner scan = new Scanner(System.in); 
+		
+		
+		String inputString = scan.nextLine(); 
+
+		scan.close(); 
+      
+		
+		System.out.println("Hello, World.");
+    System.out.println(inputString);
+        
+   
+	}
+}
