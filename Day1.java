8867b9ba5bb198e66a54dcc04f5f3e0b8d0fe927
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2020-10-25 13:25:17 +0530

    Create Day1.java

---
+++ Day1.java
@@ -0,0 +1,35 @@
+/*
+  Task : Task
+Complete the code in the editor below. The variables , , and  are already declared and initialized for you. You must:
+
+Declare  variables: one of type int, one of type double, and one of type String.
+Read  lines of input from stdin (according to the sequence given in the Input Format section below) and initialize your  variables.
+Use the  operator to perform the following operations:
+Print the sum of  plus your int variable on a new line.
+Print the sum of  plus your double variable to a scale of one decimal place on a new line.
+Concatenate  with the string you read as input and print the result on a new line. */
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+	
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+		
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        double num = scan.nextDouble();
+        scan.nextLine();
+        String str = scan.nextLine();
+        System.out.println(n+i);
+        System.out.println(num+d);
+        System.out.println(s+str);
+        
+        }
+        }
