8ff4f345bfa1ee5472b09c2249273cfe5fa9275f
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2020-11-01 20:50:30 +0530

    Create Day9.c

---
+++ Day9.c
@@ -0,0 +1,19 @@
+#include <math.h>
+#include <stdio.h>
+
+
+int factorial(int n) {
+    if(n > 1){
+        return n * factorial(n - 1);
+    }
+    else{
+        return 1;
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    printf("%d",factorial(n));
+
+}
